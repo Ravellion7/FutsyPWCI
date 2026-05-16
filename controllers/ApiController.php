78d17bb29f848c43dc06dc366c89152879f78f70
Leonardo Gabriel García Núñez <leo.g.garcia@hotmail.com>
2026-05-15 23:11:11 -0600

    open all packs fix

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -359,26 +359,34 @@ class ApiController
         $db->begin_transaction();
 
         try {
-            if (!Collector::decrementPacks($collectorId, $balance)) {
-                throw new \RuntimeException('Could not decrement packs balance.');
-            }
+    if (!Collector::decrementPacks($collectorId, $balance)) {
+        throw new \RuntimeException('Could not decrement packs balance.');
+    }
 
-            $openings = [];
-            for ($i = 0; $i < $balance; $i++) {
-                $openings[] = static::openSinglePack($collectorId, $packId, $packSize, $dropRates);
-            }
+    $openings = [];
+    for ($i = 0; $i < $balance; $i++) {
+        $opening = static::openSinglePack($collectorId, $packId, $packSize, $dropRates);
 
-            $db->commit();
+        // Remove image_url to save memory when opening multiple packs
+        foreach ($opening['cards'] as &$card) {
+            unset($card['image_url']);
+        }
+        unset($card);
 
-            static::jsonResponse([
-                'ok' => true,
-                'message' => 'All packs opened successfully.',
-                'data' => [
-                    'opened_count' => $balance,
-                    'openings' => $openings,
-                    'packs_balance' => 0,
-                ]
-            ]);
+        $openings[] = $opening;
+    }
+
+    $db->commit();
+
+    static::jsonResponse([
+        'ok' => true,
+        'message' => 'All packs opened successfully.',
+        'data' => [
+            'opened_count' => $balance,
+            'openings' => $openings,
+            'packs_balance' => 0,
+        ]
+    ]);
         } catch (\Throwable $e) {
             $db->rollback();
 
@@ -842,7 +850,7 @@ class ApiController
             ], 404);
         }
 
-        $card = \Model\Card::findByPlayerId($playerId);
+        $card = Card::findByPlayerId($playerId);
         $player['rarity'] = $card['rarity'] ?? 'Common';
 
         if (isset($player['photo']) && is_string($player['photo']) && $player['photo'] !== '') {
